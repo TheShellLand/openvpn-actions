0c5b79030de30b2c7b3ab7cd624d3a42efa648e9
naisanzaa <naisanza@gmail.com>
2023-09-02 00:43:34 +0900

    preserve sudo env vars

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 
 set -ex
 
-echo ${OPENVPN_SETUP_TOKEN}
 # Run openvpn-connector-setup to import ovpn profile and connect to VPN.
 # You will be asked to provide setup token, You can get it from the
 # Linux Connector configuration page in CloudConnexa Portal
-sudo openvpn-connector-setup
+sudo -E openvpn-connector-setup
 
 # sleep for 6 hours
 sleep 21600
