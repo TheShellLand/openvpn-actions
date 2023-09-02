a3a853bd1b0f672cf4a0d483b28aaf3bb5d109f0
naisanzaa <naisanza@gmail.com>
2023-09-02 19:10:08 +0900

    get openvpn service status

--- run.sh
+++ run.sh
@@ -7,4 +7,6 @@ set -ex
 # Linux Connector configuration page in CloudConnexa Portal
 sudo -E openvpn-connector-setup
 
-tail -f /var/log/syslog /var/log/openvpn.log /var/log/messages
+systemctl status system-openvpn3\x2dsession.slice
+
+tail -f /var/log/openvpn.log
